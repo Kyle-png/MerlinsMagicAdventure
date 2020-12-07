7d4917704b557c61b2942f84aa55242d4d9f534f
Kyle-png <56518508+Kyle-png@users.noreply.github.com>
2020-12-06 19:46:36 -0500

    Backup of template 1

--- adventure/tests.py
+++ adventure/tests.py
@@ -1,3 +1,31 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+from adventure.views import home_page
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Merlin\'s</title>', html) 
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+
+    #def test_bad_maths(self):
+        #self.assertEqual(1 + 1, 3)
+    
+
+    
--- functional_tests.py
+++ functional_tests.py
@@ -12,39 +12,41 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 
-        # 
+        # Terry heard about an online Zork like game
+        # He goes to the website.
         self.browser.get('http://localhost:8000')
 
-        # 
-        self.assertIn('To-Do', self.browser.title)
+        # He notices the Title and heading
+        self.assertIn('Merlin\'s', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)    
+        self.assertIn('Merlin\'s Adventure', header_text)    
 
-        # 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        # He sees a box inviting him to enter an action
+        inputbox = self.browser.find_element_by_id('id_action')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
+            '>'
         )
 
-        # 
-        # 
+        #It asks him to enter the character's name
+        inputbox.send_keys('Terry')
+
+        # The game tells him about his surroundings
+        # Terry is able to preform an action
         inputbox.send_keys('Obtain eye of newt')
 
-        # 
-        # "
+        # He presses enter
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_result_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Obtain eye of newt' for row in rows),
-            "New to-do item did not appear in table"
+            any(row.text == 'Terry obtained eye of newt from the shelf.' for row in rows),
+            "Nothing obtained!"
         )
 
-        # 
+        # He sees the webpage update with the text above
         # 
         self.fail('Finish the test!')
 
